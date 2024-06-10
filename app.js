2c0cf6f2967a171ae0a1803beb1c542e03c947a8
Shikhar Saxena <saxena.shikhar1808@gmail.com>
2024-06-10 22:03:31 +0530

    !gitrepo command added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const discord = require('discord.js');
 const axios = require('axios');
+const cheerio = require('cheerio');
 
 const GatewayIntentBits = discord.GatewayIntentBits;
 
@@ -16,14 +17,17 @@ client.once('ready', () =>{
 
 client.on('messageCreate', async (message) => {
     if(message.author.bot) return;
-    if(message.content.startsWith('!github')){
+    if(message.content.startsWith('!gitprofile')){
         const username = message.content.split(' ')[1];
         if(!username){
+            return message.reply('Please provide a GitHub username.');
         }
         
         try{
             const response = await axios.get(`https://api.github.com/users/${username}`);
-            const {login, avatar_url, html_url, followers, following, public_repos, public_gists} = response.data;
+            const {login, avatar_url, html_url, followers, following, public_repos, location} = response.data;
+            // console.log(response.data);
+            const contributionGraph = `https://ghchart.rshah.org/${username}`
             
             const profileEmbed = {
                 color: 0x0099ff,
@@ -53,7 +57,25 @@ client.on('messageCreate', async (message) => {
                         value: `${following}`,
                         inline: true,
                     },
+                    {
+                        name: 'Location',
+                        value: `${location || 'Not Specified'}`,
+                        inline: true,
+                    },
+                    {
+                        name: 'Contributions',
+                        value: `${contributionsCount}`,
+                        inline: true,
+                    },
+                    {
+                        name: 'Contribution Graph',
+                        value: `[Click Here](${contributionGraph})`,
+                        inline: true,
+                    },
                 ],
+                image:{
+                    url: contributionGraph,
+                },
                 timestamp: new Date(),
                 footer: {
                     text: 'GitHub Profile',
@@ -66,6 +88,53 @@ client.on('messageCreate', async (message) => {
         }catch(err){
             console.log(err);
             message.reply('Could not fetch GitHub profile. Make sure the username is correct.');
+        }
+    }
+
+    if(message.content.startsWith('!gitrepo')){
+        const username = message.content.split(' ')[1];
+        if(!username){
+            message.reply('Please provide valid username');
+            return;
+        }
+        try{
+            const response = await axios.get(`https://api.github.com/users/${username}/repos`,{
+                headers:{
+                    Authorization: `token ${process.env.GITHUB_TOKEN}`
+                },
+                params:{
+                    visibility: 'all',
+                    per_page: 100,
+                    sort: 'updated',
+
+                }
+            });
+
+            const repos = response.data;
+            if(repos.length === 0){
+                message.reply('No repositories found');
+                return;
+            }
+
+            let repoList = repos.map(repo => `[${repo.name}](${repo.html_url})`).join('\n');
+            // console.log(repoList);
+
+            const repoEmbed = {
+                color: 0x0099ff,
+                title: `${username}'s GitHub Repositories`,
+                description: repoList,
+                timestamp: new Date(),
+                footer: {
+                    text: 'GitHub Repositories',
+                    icon_url: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
+                },
+            }
+
+            message.channel.send({embeds: [repoEmbed]});
+        }
+        catch(err){
+            console.log(err);
+            message.reply('Could not fetch GitHub repositories. Make sure the username is correct.');
         }
     }
 });
